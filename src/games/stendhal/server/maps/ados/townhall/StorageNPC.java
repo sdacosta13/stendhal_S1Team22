6c812ce697770206c46cfba93a2dbc7763078153
Gabrielle Mannifield <gabrielle.mannifield@student.manchester.ac.uk>
2020-11-16 20:10:43 +0000

    Created test file for StorageNPC in Ados Town Hall
    NPC will eventually act similarly to the house sellers or bank NPCs,
    allowing players to rent storage units which behave like vaults.
    Currently only tests for dialog and not the actual storage feature.

---
+++ src/games/stendhal/server/maps/ados/townhall/StorageNPC.java
@@ -0,0 +1,16 @@
+package games.stendhal.server.maps.ados.townhall;
+
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+
+public class StorageNPC implements ZoneConfigurator {
+
+	@Override
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tests/games/stendhal/server/maps/ados/townhall/StorageNPCTest.java
@@ -0,0 +1,101 @@
+package games.stendhal.server.maps.ados.townhall;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static utilities.SpeakerNPCTestHelper.getReply;
+
+//import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import utilities.QuestHelper;
+import utilities.ZonePlayerAndNPCTestImpl;
+
+public class StorageNPCTest extends ZonePlayerAndNPCTestImpl {
+
+	private static final String ZONE_NAME = "int_ados_town_hall_3";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		setupZone(ZONE_NAME);
+	}
+	
+	public StorageNPCTest() {
+		setNpcNames("Serena");
+		setZoneForPlayer(ZONE_NAME);
+		addZoneConfigurator(new StorageNPC(), ZONE_NAME);
+	}
+	
+	/**
+	 * Tests for hi and job and bye.
+	 */
+	@Test
+	public void testJob() {
+
+		final SpeakerNPC npc = getNPC("Serena");
+		final Engine en = npc.getEngine();
+
+		en.step(player, "hi");
+		assertTrue(npc.isTalking());
+		assertEquals(
+				"Hey there.",
+				getReply(npc));
+		en.step(player, "job");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "I rent out storage units for people who just have too much stuff!",
+		            getReply(npc));
+		en.step(player, "bye");
+		assertFalse(npc.isTalking());
+	}
+	/**
+	 * Tests for smaller storage units.
+	 */
+	@Test
+	public void testSmallStorage() {
+
+		final SpeakerNPC npc = getNPC("Serena");
+		final Engine en = npc.getEngine();
+
+		en.step(player, "hi");
+		assertTrue(npc.isTalking());
+		assertEquals(
+				"Hey there.",
+				getReply(npc));
+		en.step(player, "storage");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "You can store whatever you like in your storage unit, but don't forget to pay your rent in advance! A small unit can hold three chests and costs 200 money per week, while a large unit can hold six chests and costs 300 money per week.",
+		            getReply(npc));
+		en.step(player, "rent");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "You must always pay your rent in advance, or your unit will be emptied and your items left out for the public.",
+		            getReply(npc));
+		en.step(player, "small");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "You'd like a small storage unit? That costs 200 money per week. How many weeks would you like to pay for?",
+		            getReply(npc));
+		en.step(player, "4");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "That will be 800 money. Are you sure you want to rent?",
+		            getReply(npc));
+		// need to make sure player has money, and set up portal to a storage unit
+		
+		en.step(player, "rent");
+		assertTrue(npc.isTalking());
+		assertEquals(
+		            "Great! Here's your key."
+		            + " Do you want to buy a spare key for 1000 money?",
+		            getReply(npc));
+		en.step(player, "bye");
+		assertFalse(npc.isTalking());
+	}
+	
+}
