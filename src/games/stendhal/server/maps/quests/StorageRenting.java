d1011ea7edcf17e1f8b5abac3d6b42e12edbe55d
Gabrielle Mannifield <gabrielle.mannifield@student.manchester.ac.uk>
2020-11-17 19:29:26 +0000

    Edited the StorageRenting file to add functionality
    This won't work yet, as StorageUnits and the NPC haven't been added.
    Eventually, it will act like 'HouseBuying' but for storage units.

--- src/games/stendhal/server/maps/quests/StorageRenting.java
+++ src/games/stendhal/server/maps/quests/StorageRenting.java
@@ -1,10 +1,53 @@
 package games.stendhal.server.maps.quests;
 
-public class StorageRenting {
+import java.util.LinkedList;
 
-	public void addToWorld() {
-		// TODO Auto-generated method stub
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.quests.houses.HouseBuyingMain;
+
+public class StorageRenting extends AbstractQuest{
+	private static final String QUEST_SLOT = "storage";
+	private HouseBuyingMain quest;
+	
+	@Override
+	public String getSlotName() {
+		return QUEST_SLOT;
+	}
+	
+	@Override
+	public void addToWorld() { //##### This section needs changing
+		quest = new HouseBuyingMain();
+		quest.addToWorld();
+		
+		fillQuestInfo(
+				"Storage Renting",
+				"You can rent a storage unit in Ados to hold your belongings.",
+				false);
 		
 	}
-
+	
+	@Override
+	public LinkedList<String> getHistory(final Player player) {
+		return quest.getHistory(player);
+	}
+	
+	@Override
+	public String getName() {
+		return "StorageRenting";
+	}
+	
+	@Override
+	public int getMinLevel() {
+		return 0;
+	}
+	
+	@Override
+	public boolean isCompleted(final Player player) {
+		return quest.isCompleted(player);
+	}
+	
+	@Override
+	public String getNPCName() {
+		return "Serena";
+	}
 }
