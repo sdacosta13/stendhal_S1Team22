39fccd405c7023b76775f326ec9b1fb6f5a297a1
Yanyi Li <yanyi.li@student.manchester.ac.uk>
2020-11-17 21:14:25 +0000

    the implementation class has been finished and test passed

--- src/games/stendhal/server/maps/deniran/DeniranFerry.java
+++ src/games/stendhal/server/maps/deniran/DeniranFerry.java
@@ -1,5 +1,6 @@
 package games.stendhal.server.maps.deniran;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import games.stendhal.common.Direction;
@@ -11,136 +12,131 @@ import games.stendhal.server.entity.player.Player;
 
 
 
-public final class DeniranFerry implements TurnListener{
+public final class DeniranFerry implements TurnListener {
 	private static DeniranFerry instance;
 	private Status current;
-	
-	private static StendhalRPZone isDeniranZone;
-	private static StendhalRPZone AdosZone;
-	
-	private DeniranFerry() {
-			current = Status.ANCHORED_AT_ADOS;
-		
-			}
+	private final List<IFerryListener> listeners;
+	/*
+	 * private static StendhalRPZone isDeniranZone; private static StendhalRPZone
+	 * AdosZone; private static StendhalRPZone ShipZone;
+	 */
+	// protected Status ferrystate;
+
+	public DeniranFerry() {
+		listeners = new LinkedList<IFerryListener>();
+		current = Status.ANCHORED_AT_ADOS;
+
+	}
+
 	public static DeniranFerry get() {
-		if(instance == null) {
+		if (instance == null) {
 			DeniranFerry instance = new DeniranFerry();
 			// initiate the turn notification cycle
 			SingletonRepository.getTurnNotifier().notifyInSeconds(1, instance);
 			DeniranFerry.instance = instance;
 		}
 		return instance;
-		
+
 	}
+
 	public Status getState() {
-		return this.current;
+		return current;
 	}
 
-
-	public enum Status{
-		ANCHORED_AT_DENIRAN{
+	public enum Status {
+		ANCHORED_AT_DENIRAN {
 			@Override
-			Status next(){
+			Status next() {
 				return DRIVING_TO_ADOS;
 			}
+
 			@Override
 			int duration() {
-				
-				return 20;
+
+				return 20;//Set duration time to 20 for convenience
 			}
+		
 		},
-		ANCHORED_AT_ADOS{
+		ANCHORED_AT_ADOS {
 			@Override
-			Status next(){
+			Status next() {
 				return DRIVING_TO_DENIRAN;
 			}
+
 			@Override
 			int duration() {
 				return 20;
 			}
 		},
-		DRIVING_TO_ADOS{
+		DRIVING_TO_ADOS {
 			@Override
-			Status next(){
+			Status next() {
 				return ANCHORED_AT_ADOS;
 			}
+
 			@Override
 			int duration() {
 				return 20;
 			}
 		},
-		DRIVING_TO_DENIRAN(){
+		DRIVING_TO_DENIRAN() {
 			@Override
-			Status next(){
+			Status next() {
 				return ANCHORED_AT_DENIRAN;
 			}
+
 			@Override
 			int duration() {
 				return 20;
 			}
 		};
-	
+
 		abstract Status next();
+
 		abstract int duration();
 	}
-	
-	private StendhalRPZone getIsDeniranZone() {
-		if (isDeniranZone == null) {
 
-			isDeniranZone = SingletonRepository.getRPWorld().getZone("0_deniran_river_s"); //modify later
-		}
-
-		return isDeniranZone;
-	}
-	
-	
-	private static StendhalRPZone getAdosZone() {
-		if (AdosZone== null) {
-			 AdosZone = SingletonRepository.getRPWorld().getZone("0_ados_coast_s_w2");//modify later
-		}
-		return AdosZone;
-	}
-	
-	
-	public void DisembarkPlayer(final Player player) {
-		switch (current) {
-		case ANCHORED_AT_DENIRAN:
-			player.teleport(getIsDeniranZone(), 100, 100, Direction.LEFT, null);
-			
-			break;
-		case ANCHORED_AT_ADOS:
-			player.teleport(getAdosZone(), 16, 89, Direction.LEFT, null);
-			
-			break;
-
-		default:
-			break;
-
-	}
-
-}
 	@Override
-	public void onTurnReached(int currentTurn) {
-			// next state trigger
-			current = current.next();
-			SingletonRepository.getTurnNotifier().notifyInSeconds(current.duration(), this);
-			if(current == Status.ANCHORED_AT_DENIRAN) {
-				StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_deniran_river_s");
-				List<Player> players = zone.getPlayers();
-				for(int i =0;i<players.size();i++) {
-					players.get(i).teleport("0_deniran_river_s",100,100,Direction.LEFT,null);
-				}
-				//player.teleport("0_deniran_river_s",100,100,Direction.LEFT,null);
-			}
-			else if(current == Status.ANCHORED_AT_ADOS) {
-				StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_ados_coast_s_w2");
-				List<Player> players = zone.getPlayers();
-				for(int i =0;i<players.size();i++) {
-					players.get(i).teleport("0_deniran_river_s",100,100,Direction.LEFT,null);
-				}
-			}	
-			
-		}
-	
-}
+	public void onTurnReached(final int currentTurn) {
 
+		current = current.next();
+		for (final IFerryListener npc : listeners) {
+			npc.onNewFerryState(current);
+		}
+		
+		if (current==Status.ANCHORED_AT_DENIRAN) {
+			StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_deniran_ship_w2");
+			List<Player> players = zone.getPlayers();
+			for (int i = 0; i < players.size(); i++) {
+			players.get(i).teleport("0_deniran_river_s", 100, 100, Direction.LEFT, null); 
+			}
+			 
+			
+		} else if (current==Status.ANCHORED_AT_ADOS) {
+			StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_deniran_ship_w2");
+			List<Player> players = zone.getPlayers();
+			for (int i = 0; i < players.size(); i++) {
+				players.get(i).teleport("0_ados_coast_s_w2", 100, 100, Direction.LEFT, null);
+			}
+		}
+		SingletonRepository.getTurnNotifier().notifyInSeconds(current.duration(), this);
+
+	}
+	public void addListener(final IFerryListener npc) {
+		listeners.add(npc);
+	}
+
+	public abstract static class FerryListener implements IFerryListener {
+		public FerryListener() {
+			SingletonRepository.getDeniranFerry().addListener(this);
+		}
+
+
+	}
+
+	public interface IFerryListener {
+		void onNewFerryState(Status current);
+	}
+
+
+}
