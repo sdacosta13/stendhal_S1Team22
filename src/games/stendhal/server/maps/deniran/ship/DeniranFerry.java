c7c1ea203665f9b9db7d120273055c87343ff4ad
Yonggang Wang <yonggang.wang-2@student.manchester.ac.uk>
2020-11-18 17:52:56 +0800

    Reformat the style of this file to make it more readable, and add some feature with them commented which can be used in the future releases.

--- src/games/stendhal/server/maps/deniran/ship/DeniranFerry.java
+++ src/games/stendhal/server/maps/deniran/ship/DeniranFerry.java
@@ -8,19 +8,20 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.events.TurnListener;
 import games.stendhal.server.entity.player.Player;
-
+//import games.stendhal.server.util.TimeUtil;
 
 
 
 public final class DeniranFerry implements TurnListener {
+	
 	private static DeniranFerry instance;
 	private Status current;
-	private final List<IFerryListener> listeners;
-	/*
-	 * private static StendhalRPZone isDeniranZone; private static StendhalRPZone
-	 * AdosZone; private static StendhalRPZone ShipZone;
+	
+	/**
+	 * A list of non-player characters that get notice when the ferry arrives or
+	 * departs, so that they can react accordingly, e.g. inform nearby players.
 	 */
-	// protected Status ferrystate;
+	private final List<IFerryListener> listeners;
 
 	public DeniranFerry() {
 		listeners = new LinkedList<IFerryListener>();
@@ -42,63 +43,26 @@ public final class DeniranFerry implements TurnListener {
 	public Status getState() {
 		return current;
 	}
+	
+//	/**
+//	 * Gets a textual description of the ferry's status.
+//	 * This method may be useful to future releases if we have NPCs.
+//	 * @return A String representation of time remaining till next state.
+//	 */
+//
+//	private String getRemainingSeconds() {
+//		final int secondsUntilNextState = SingletonRepository.getTurnNotifier()
+//				.getRemainingSeconds(this);
+//		return TimeUtil.approxTimeUntil(secondsUntilNextState);
+//	}
 
-	public enum Status {
-		ANCHORED_AT_DENIRAN {
-			@Override
-			Status next() {
-				return DRIVING_TO_ADOS;
-			}
-
-			@Override
-			int duration() {
-
-				return 20;//Set duration time to 20 for convenience
-			}
-		
-		},
-		ANCHORED_AT_ADOS {
-			@Override
-			Status next() {
-				return DRIVING_TO_DENIRAN;
-			}
-
-			@Override
-			int duration() {
-				return 20;
-			}
-		},
-		DRIVING_TO_ADOS {
-			@Override
-			Status next() {
-				return ANCHORED_AT_ADOS;
-			}
-
-			@Override
-			int duration() {
-				return 20;
-			}
-		},
-		DRIVING_TO_DENIRAN() {
-			@Override
-			Status next() {
-				return ANCHORED_AT_DENIRAN;
-			}
-
-			@Override
-			int duration() {
-				return 20;
-			}
-		};
-
-		abstract Status next();
-
-		abstract int duration();
-	}
-
+	/**
+	 * Is called when the ferry has either arrived at or departed from a harbor.
+	 * @param currentTurn the turn when this listener is called
+	 */
 	@Override
 	public void onTurnReached(final int currentTurn) {
-
+		
 		current = current.next();
 		for (final IFerryListener npc : listeners) {
 			npc.onNewFerryState(current);
@@ -106,12 +70,11 @@ public final class DeniranFerry implements TurnListener {
 		
 		if (current==Status.ANCHORED_AT_DENIRAN) {
 			StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_deniran_ship_w2");
+
 			List<Player> players = zone.getPlayers();
 			for (int i = 0; i < players.size(); i++) {
 			players.get(i).teleport("0_deniran_river_s", 100, 100, Direction.LEFT, null); 
 			}
-			 
-			
 		} else if (current==Status.ANCHORED_AT_ADOS) {
 			StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_deniran_ship_w2");
 			List<Player> players = zone.getPlayers();
@@ -126,17 +89,99 @@ public final class DeniranFerry implements TurnListener {
 		listeners.add(npc);
 	}
 
+	
+	/**
+	 * Auto registers the listener to Deniranferry.
+	 * deregistration must be implemented if it is used for short living objects
+	 *
+	 */
 	public abstract static class FerryListener implements IFerryListener {
 		public FerryListener() {
 			SingletonRepository.getDeniranFerry().addListener(this);
 		}
-
 
 	}
 
 	public interface IFerryListener {
 		void onNewFerryState(Status current);
 	}
+	
+	// toString method is useful for future releases, and is used by getRemainingSeconds method.
+	public enum Status {
+		ANCHORED_AT_ADOS {
+			@Override
+			Status next() {
+				return DRIVING_TO_DENIRAN;
+			}
+			
+			@Override
+			int duration() {
+				return 20;
+			}
+			
+//			@Override
+//			public String toString() {
+//				return "The ferry is currently anchored at the mainland. It will take off in "
+//						+ SingletonRepository.getDeniranFerry().getRemainingSeconds() + ".";
+//			}
+		},
+		DRIVING_TO_DENIRAN() {
+			@Override
+			Status next() {
+				return ANCHORED_AT_DENIRAN;
+			}
+			
+			@Override
+			int duration() {
+				return 20;
+			}
+			
+//			@Override
+//			public String toString() {
+//				return "The ferry is currently sailing to Deniran. It will arrive in "
+//						+ SingletonRepository.getDeniranFerry().getRemainingSeconds() + ".";
+//			}
+		},
+		ANCHORED_AT_DENIRAN {
+			@Override
+			Status next() {
+				return DRIVING_TO_ADOS;
+			}
 
+			@Override
+			int duration() {
+
+				return 20;//Set duration time to 20 for convenience
+			}
+			
+//			@Override
+//			public String toString() {
+//				return "The ferry is currently anchored at Deniran. It will take off in "
+//						+ SingletonRepository.getDeniranFerry().getRemainingSeconds() + ".";
+//			}
+		
+		},
+		DRIVING_TO_ADOS {
+			@Override
+			Status next() {
+				return ANCHORED_AT_ADOS;
+			}
+
+			@Override
+			int duration() {
+				return 20;
+			}
+			
+//			@Override
+//			public String toString() {
+//				return "The ferry is currently sailing to the mainland. It will arrive in "
+//						+ SingletonRepository.getDeniranFerry().getRemainingSeconds() + ".";
+//			}
+		};
+
+		abstract Status next();
+
+		abstract int duration();
+	}
 
 }
