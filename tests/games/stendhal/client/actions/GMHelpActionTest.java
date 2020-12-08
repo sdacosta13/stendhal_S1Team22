735784b8dfd205ad9adcc24e85e4210505baf331
yanan.zhang <yanan.zhang-3@student.manchester.ac.uk>
2020-12-08 17:35:42 +0000

    Edit the GMHelpActionTest and HelpActionTest
    Add the function for testing the contents of the "line". Change to use
    the SlashFunction as well.

--- tests/games/stendhal/client/actions/GMHelpActionTest.java
+++ tests/games/stendhal/client/actions/GMHelpActionTest.java
@@ -3,50 +3,100 @@ package games.stendhal.client.actions;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import games.stendhal.client.ClientSingletonRepository;
 import games.stendhal.client.StendhalClient;
+import games.stendhal.client.gui.MockUserInterface;
+import games.stendhal.client.gui.UserInterface;
 import games.stendhal.client.util.UserInterfaceTestHelper;
-//import marauroa.common.game.RPAction;
 
 public class GMHelpActionTest {
-	@BeforeClass
-	public static void init() {
-		UserInterfaceTestHelper.resetUserInterface();
-	}
+   @BeforeClass
+   public static void init() {
+	   UserInterfaceTestHelper.resetUserInterface();
+   }
+ 
+   @After
+   public void tearDown() throws Exception {
+	   StendhalClient.resetClient();
+   }
+ 
+   private static MockUserInterface getInterface() {
+	   UserInterface ui = ClientSingletonRepository.getUserInterface();
+	   if (ui instanceof MockUserInterface) {
+		   return (MockUserInterface) ui;
+	   }
+	   fail();
+	   return null;
+   }
+    
+   /**
+    * Tests for execute.
+    */
+   @Test
+   public void testExecuteIf() { //if params[0] == null
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertTrue(action.execute(new String[] {null},null));
+	   assertEquals("\t\tList the jailed players and their sentences.",getInterface().getLastEventLine());
+   }
+   
+   @Test
+   public void testExecuteElseIf() { //if params[0] == "alter"
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertTrue(action.execute(new String[] {"alter"},null));
+	   assertEquals("\t\t  This will make <testplayer> look like danter" ,getInterface().getLastEventLine());
+  }
+    
+   @Test
+   public void testExecuteElesIfAgain() { //if params[0] == "script"
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertTrue(action.execute(new String[] {"script"},null));
+	   assertEquals("#/script #ResetSlot.class #player #slot : Resets the named slot such as !kills or !quests. Useful for debugging.",getInterface().getLastEventLine());
+  }
+   
 
-	@After
-	public void tearDown() throws Exception {
-		StendhalClient.resetClient();
-	}
-
-	/**
-	 * Tests for execute.
-	 */
-	@Test
-	public void testExecute() {
-		final GMHelpAction action = new GMHelpAction();
-
-		assertTrue(action.execute(new String[]{"alter"}, null));
-		assertTrue(action.execute(new String[]{"support"}, null));
-		assertTrue(action.execute(new String[]{"script"}, null));
-		assertTrue(action.execute(new String[]{null}, null));
-  
-		assertFalse(action.execute(new String[]{"alter", "support"}, null));
-		assertFalse(action.execute(new String[]{"alter", "support", "script"}, null));
-  
-	}
-
-	/**
+   @Test
+   public void testExecuteElseIfThird() { //test if params[0] == ""
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertTrue(action.execute(new String[] {"support"},null));
+	   //assertEquals(,getInterface().getLastEventLine()); // need to be achieved
+  }
+   
+   @Test
+   public void testExecuteElseIfFourth() { //if params[0] != "script" ,"alter" ,"support"
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertFalse(action.execute(new String[]{"aler"}, null));
+	   assertFalse(action.execute(new String[]{"suport"}, null));		
+	   assertFalse(action.execute(new String[]{"scrpt"}, null));
+  }
+   
+   @Test
+   public void testExecuteElse() {//test if params length !=1
+	   SlashActionRepository.register();
+	   final SlashAction action = SlashActionRepository.get("gmhelp");
+	   assertFalse(action.execute(new String[]{"alter", "support"}, null));
+	   assertFalse(action.execute(new String[]{"alter", "support", "script"}, null));
+   
+  }
+   
+   	/**
 	 * Tests for getMaximumParameters.
 	 */
 	@Test
 	public void testGetMaximumParameters() {
-		final GMHelpAction action = new GMHelpAction();
+		SlashActionRepository.register();
+		final SlashAction action = SlashActionRepository.get("gmhelp");
 		assertEquals(1, action.getMaximumParameters());
 	}
 
@@ -55,7 +105,8 @@ public class GMHelpActionTest {
 	 */
 	@Test
 	public void testGetMinimumParameters() {
-		final GMHelpAction action = new GMHelpAction();
+		SlashActionRepository.register();
+		final SlashAction action = SlashActionRepository.get("gmhelp");
 		assertEquals(0, action.getMinimumParameters());
 	}
 }
--- tests/games/stendhal/client/actions/HelpActionTest.java
+++ tests/games/stendhal/client/actions/HelpActionTest.java
@@ -4,33 +4,48 @@ package games.stendhal.client.actions;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 //import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import games.stendhal.client.ClientSingletonRepository;
 import games.stendhal.client.StendhalClient;
+import games.stendhal.client.gui.MockUserInterface;
+import games.stendhal.client.gui.UserInterface;
 import games.stendhal.client.util.UserInterfaceTestHelper;
 
 public class HelpActionTest {
-	@BeforeClass
-	public static void init() {
-		UserInterfaceTestHelper.resetUserInterface();
-	}
+	 @BeforeClass
+	 public static void init() {
+		 UserInterfaceTestHelper.resetUserInterface();
+	 }
 
-	@After
-	public void tearDown() throws Exception {
-		StendhalClient.resetClient();
-	}
+	 @After
+	 public void tearDown() throws Exception {
+		 StendhalClient.resetClient();
+	 }
+
+	 private static MockUserInterface getInterface() {
+		 UserInterface ui = ClientSingletonRepository.getUserInterface();
+		 if (ui instanceof MockUserInterface) {
+			 return (MockUserInterface) ui;
+		 }
+		 fail();
+		 return null;
+	 }
 
 	/**
 	 * Tests for execute.
 	 */
 	@Test
 	public void testExecute() {
-		final HelpAction action = new HelpAction();
-		// 
+		SlashActionRepository.register();
+		final SlashAction action = SlashActionRepository.get("help");
+
 		assertTrue(action.execute(new String[]{null}, null));
+		assertEquals("- /" + "removedetail" + " \tRemove the detail layer (e.g. balloon, umbrella, etc.) from character.",getInterface().getLastEventLine());
 	}
 
 	/**
@@ -38,7 +53,8 @@ public class HelpActionTest {
 	 */
 	@Test
 	public void testGetMaximumParameters() {
-		final HelpAction action = new HelpAction();
+		SlashActionRepository.register();
+		final SlashAction action = SlashActionRepository.get("help");
 		assertEquals(0, action.getMaximumParameters());
 	}
 
@@ -47,7 +63,8 @@ public class HelpActionTest {
 	 */
 	@Test
 	public void testGetMinimumParameters() {
-		final HelpAction action = new HelpAction();
+		SlashActionRepository.register();
+		final SlashAction action = SlashActionRepository.get("help");
 		assertEquals(0, action.getMinimumParameters());
 	}
 }
